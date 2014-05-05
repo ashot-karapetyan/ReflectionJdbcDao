dab7f5ec4366db4d72bea6768a6b50fa413386c9
Ashot Karapetyan <ashmath@mail.ru>
2014-05-06 00:47:12 +0400

    add db_table create script

---
+++ DB_Table_Create.sql
@@ -0,0 +1,24 @@
+USE [ReflectionJdbcTestDB]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Projects](
+	[id] [int] NOT NULL,
+	[name] [nchar](200) NOT NULL,
+	[date] [datetime] NOT NULL,
+	[amount] [decimal](18, 2) NOT NULL,
+	[description] [nchar](500) NULL,
+	[flag] [bit] NOT NULL,
+ CONSTRAINT [PK_Projects] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
